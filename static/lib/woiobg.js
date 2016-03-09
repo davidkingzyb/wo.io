5d850be646bb7d948af6bfbf52ff4fd7d121b435
DKZ <davidkingzyb@163.com>
2016-03-10 01:11:51 +0800

    head load

--- static/lib/woiobg.js
+++ static/lib/woiobg.js
@@ -1,8 +1,3 @@
-"use strict";
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /////////////////////////////////////////////////
 //                                             // 
@@ -14,38 +9,61 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 /////////////////////////////////////////////////
 //  2016/03/05 by DKZ https://davidkingzyb.github.io
 
-var woioBg = function () {
-    function woioBg() {
-        _classCallCheck(this, woioBg);
+var woioBg=function(){
+    this.scene=new THREE.Scene(),
+    this.camera=new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000),
+    this.renderer=new THREE.WebGLRenderer(),
+    this.axes=new THREE.AxisHelper(20),
+    this.ambientlight=new THREE.AmbientLight(0x0c0c0c)
+}
+woioBg.prototype={
+    init:function(){
+        this.renderer.setClearColor(new THREE.Color(0xEEEEEE));
+        this.renderer.setSize(window.innerWidth,window.innerHeight);
+        this.scene.add(this.axes);
 
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.renderer = new THREE.WebGLRenderer();
-        this.axes = new THREE.AxisHelper(20);
+        this.camera.position.x=-30;
+        this.camera.position.y=40;
+        this.camera.position.z=30;
+        this.camera.lookAt(this.scene.position);
 
-        this.init();
+        this.scene.add(this.ambientlight);
+
+        document.getElementById("WebGL-output").appendChild(this.renderer.domElement);
+
+        this.renderer.render(this.scene,this.camera);
+    },
+    loadHead:function(){
+        var loader=new THREE.OBJLoader();
+        var scene=this.scene;
+        var renderer=this.renderer;
+        var camera=this.camera;
+        loader.load('../static/res/woioLow.obj',function(geometry){ 
+            var material=new THREE.MeshBasicMaterial({color:0x666666});
+            geometry.children[0].material=material;
+            scene.add(geometry);
+            renderer.render(scene,camera);
+
+        })
     }
+}
+window.onload=function(){
+    var woiobg=new woioBg();
+    woiobg.init();
+    woiobg.loadHead();
 
-    _createClass(woioBg, [{
-        key: "init",
-        value: function init() {
-            this.renderer.setClearColor(new THREE.Color(0xEEEEEE));
-            this.renderer.setSize(window.innerWidth, window.innerHeight);
-            this.scene.add(this.axes);
+    var trackballcontrols=new THREE.TrackballControls(woiobg.camera);
+        trackballcontrols.rotateSpeed=1;
+        trackballcontrols.zoomSpeed=1;
+        trackballcontrols.panSpeed=1;
 
-            this.camera.position.x = -30;
-            this.camera.position.y = 40;
-            this.camera.position.z = 30;
-            this.camera.lookAt(this.scene.position);
-            document.getElementById("WebGL-output").appendChild(this.renderer.domElement);
+    var clock=new THREE.Clock();
+    function render(){
+        var delta=clock.getDelta();
+        trackballcontrols.update(delta);
+        requestAnimationFrame(render);
+        woiobg.renderer.render(woiobg.scene,woiobg.camera);
+    }
+    render();
 
-            this.renderer.render(this.scene, this.camera);
-        }
-    }]);
-
-    return woioBg;
-}();
-
-window.onload = function () {
-    var woiobg = new woioBg();
-};
+}
