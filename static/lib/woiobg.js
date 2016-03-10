949e231507a7aad8d09ccaa05a9f2e4b246288da
DKZ <davidkingzyb@163.com>
2016-03-11 00:20:15 +0800

    high test

--- static/lib/woiobg.js
+++ static/lib/woiobg.js
@@ -10,16 +10,19 @@
 //  2016/03/05 by DKZ https://davidkingzyb.github.io
 
 var woioBg=function(){
-    this.scene=new THREE.Scene(),
-    this.camera=new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000),
-    this.renderer=new THREE.WebGLRenderer(),
-    this.axes=new THREE.AxisHelper(20),
-    this.ambientlight=new THREE.AmbientLight(0x0c0c0c)
+    this.scene=new THREE.Scene();
+    this.camera=new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
+    this.renderer=new THREE.WebGLRenderer();
+    this.axes=new THREE.AxisHelper(20);
+    this.ambientlight=new THREE.AmbientLight(0x0c0c0c);
+    this.spotlight=new THREE.SpotLight(0xffffff);
 }
 woioBg.prototype={
     init:function(){
-        this.renderer.setClearColor(new THREE.Color(0xEEEEEE));
+        this.renderer.setClearColor(new THREE.Color(0xEEEEEE),1.0);
         this.renderer.setSize(window.innerWidth,window.innerHeight);
+        this.renderer.shadowMapEnabled=true;
+
         this.scene.add(this.axes);
 
         this.camera.position.x=-30;
@@ -28,6 +31,10 @@ woioBg.prototype={
         this.camera.lookAt(this.scene.position);
 
         this.scene.add(this.ambientlight);
+
+        this.spotlight.position.set(0,100,100);
+        this.spotlight.castShadow=true;
+        this.scene.add(this.spotlight);
 
         document.getElementById("WebGL-output").appendChild(this.renderer.domElement);
 
@@ -38,12 +45,28 @@ woioBg.prototype={
         var scene=this.scene;
         var renderer=this.renderer;
         var camera=this.camera;
-        loader.load('../static/res/woioLow.obj',function(geometry){ 
-            var material=new THREE.MeshBasicMaterial({color:0x666666});
-            geometry.children[0].material=material;
-            scene.add(geometry);
+        loader.load('../static/res/woioHigh.obj',function(o){ 
+            var materials=[new THREE.MeshLambertMaterial({color:0xaaaaaa})];
+            var geom=o.children[0].geometry
+            var mesh=THREE.SceneUtils.createMultiMaterialObject(geom,materials);
+            mesh.castShadow=true;
+            scene.add(mesh);
             renderer.render(scene,camera);
 
+        })
+    },
+    loadEye:function(){
+        var loader=new THREE.OBJLoader();
+        var scene=this.scene;
+        var renderer=this.renderer;
+        var camera=this.camera;
+        loader.load('../static/res/woioEye.obj',function(o){
+            var materials=[new THREE.MeshLambertMaterial({color:0x777777})];
+            var geom=o.children[0].geometry
+            var mesh=THREE.SceneUtils.createMultiMaterialObject(geom,materials);
+            mesh.castShadow=true;
+            scene.add(mesh);
+            renderer.render(scene,camera);
         })
     }
 }
@@ -51,6 +74,7 @@ window.onload=function(){
     var woiobg=new woioBg();
     woiobg.init();
     woiobg.loadHead();
+    woiobg.loadEye();
 
     var trackballcontrols=new THREE.TrackballControls(woiobg.camera);
         trackballcontrols.rotateSpeed=1;
