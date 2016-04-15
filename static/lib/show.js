3b8c515362bb9876630307ae6d627f26bc85bf48
DKZ <davidkingzyb@163.com>
2016-04-15 19:03:38 +0800

    test

--- static/lib/show.js
+++ static/lib/show.js
@@ -7,3 +7,19 @@
 //  |___/\___| \_____/  |____|  |__| \_____/   //  
 /////////////////////////////////////////////////
 //  2016/04/15 by DKZ https://davidkingzyb.github.io
+var show=(function(){
+    function show(){
+
+    };
+    show.init=function(woiobg){
+        this.woiobg=woiobg;
+        console.log(woiobg)
+    }
+    show.randomColor=function(){
+        return parseInt(Math.random()*0xFFFFFF,16);
+    };
+    show.changeEyeColor=function(color){
+        console.log(this.woiobg)
+    }
+    return show;
+})();
